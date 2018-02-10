603af7509b408054e985abe4e82d30e2f7eb1f08
qiwsir <qiwsir@gmail.com>
2018-02-10 21:44:14 +0800

    first program

---
+++ PythonBase/firstprogram.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+
+"""
+    This is my first program by Python.
+    I am learning Python with Laoqi.
+"""
+print("Hello, World")
+n = input("please input your age:")    # n is a str
+n = float(n)    # convert to float
+result = n + 100
+print("After 100 years, Your age is: ", result)
