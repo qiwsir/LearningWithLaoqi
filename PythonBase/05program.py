37e4360ba181004a85f4a9078bfb05da629c310a
qiwsir <qiwsir@gmail.com>
2018-02-17 20:13:24 +0800

    the file

---
+++ PythonBase/05program.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+def add_func(a, b):
+    """
+    This is a function. It is about add two object.
+    """
+    c = a + b
+    return c
+
+if __name__ == "__main__":
+    x = 3
+    y = 4
+    r = add_func(x, y)
+    print("the result is: ", r)
