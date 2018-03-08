a92e98514f5501dff4b25e683720f48813a905b8
qiwsir <qiwsir@gmail.com>
2018-03-08 13:14:21 +0800

    11

---
+++ PythonAdvance/1101pyad.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+
+class RoundFloat2:
+    def __init__(self, val):
+        assert isinstance(val, float), "value must be a float."
+        self.value = round(val, 2)
+    
+    def __str__(self):
+        return "{0:.2f}".format(self.value)
+
+    __repr__ = __str__
+
+if __name__ == "__main__":
+    r = RoundFloat2(3.1415926)
+    print(r)
+    print(type(r))
